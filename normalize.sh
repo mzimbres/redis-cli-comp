d3ae557f89b6377df2a01d031e2965da944a20c2
Marcelo Zimbres <mzimbres@gmail.com>
2025-12-14 23:13:46 +0100

    Progresses with scripts

--- normalize.sh
+++ normalize.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+/usr/bin/time --format="%E %U %S" ./go-redis/app >& /dev/null
+
 awk '
-   NR==1 { min=$2 }
+   NR==1{min=$2}
    FNR==NR{min=($2+0<min)?$2:min;next}
-   {print $1,$2/min}
+   {printf "%s %.3f %.3f %.3f\n", $1,$2/min,$3/min,$4/min}
 ' time.txt time.txt
