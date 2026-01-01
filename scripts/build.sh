492e32183aec51549e3a3742e7d856e87233dc56
Marcelo Zimbres <mzimbres@gmail.com>
2026-01-02 00:38:48 +0100

    Progresses with the build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 
-format="%e %U %S %P"
 root=$(pwd)
-apps_dir=$root/apps
+apps=$root/apps
+prefix=$root/tmp
 
 #--------------------------------------------
-mkdir -p $root/tmp
-cd $root/tmp
-cmake -S $apps_dir/boost-redis -B . -DCMAKE_BUILD_TYPE=Release
-make -j2
+# boost-redis
+
+boost_redis_builddir=$root/boost_redis
+mkdir -p $boost_redis_builddir
+cd $boost_redis_builddir
+cmake\
+  -S $apps/boost-redis\
+  -B $boost_redis_builddir\
+  -DCMAKE_INSTALL_PREFIX=$prefix\
+  -DCMAKE_BUILD_TYPE=Release
+
+cmake --build $boost_redis_builddir --target install -j3
 
 #--------------------------------------------
-cd $apps_dir/go-redis
+# redis-rs
+
+cargo install --path $apps/redis-rs --root $prefix
+
+#--------------------------------------------
+# go-redis
+
+cd $apps/go-redis
 go get
 go build 
+cp app $prefix/bin/app_go_redis
 
 #--------------------------------------------
-cd $apps_dir/rueidis
+# rueidis
+
+cd $apps/rueidis
 go get
 go build 
+cp app $prefix/bin/app_rueidis
 
 #--------------------------------------------
-cd $apps_dir/redis-rs
-cargo build --release
+# fred-rs
 
-#--------------------------------------------
-cd $apps_dir/fred-rs
-cargo build --release
+cargo install --path $apps/fred-rs --root $prefix
 
 # Benchmarks
 echo ""
@@ -34,35 +50,13 @@ echo "Running benchamrks"
 echo "real user sys cpu"
 echo ""
 
-echo "========================================================="
-echo "boost-redis"
-cd $root/tmp
-/usr/bin/time --format="$format" ./app
-pidstat -v 1 -e ./app
-pidstat -u 1 -e ./app
-pidstat -w 1 -e ./app
+apps="boost_redis redis_rs go_redis rueidis"
+for app in $apps; do
+   echo "$app"
+   /usr/bin/time --format="%e %U %S %P" $prefix/bin/app_$app
+done
 
-echo "========================================================="
-echo "go-redis"
-cd $apps_dir/go-redis
-/usr/bin/time --format="$format" ./app
-pidstat -v 1 -e ./app
-pidstat -u 1 -e ./app
-pidstat -w 1 -e ./app
-
-echo "========================================================="
-echo "rueidis"
-cd $apps_dir/rueidis
-/usr/bin/time --format="$format" ./app
-pidstat -v 1 -e ./app
-pidstat -u 1 -e ./app
-pidstat -w 1 -e ./app
-
-echo "========================================================="
-echo "redis-rs"
-cd $apps_dir/redis-rs/target/release
-/usr/bin/time --format="$format" ./app
-pidstat -v 1 -e ./app
-pidstat -u 1 -e ./app
-pidstat -w 1 -e ./app
-
+#pidstat -v 1 -e ./app
+#pidstat -u 1 -e ./app
+#pidstat -w 1 -e ./app
+# perf stat
