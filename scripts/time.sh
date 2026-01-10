f99f21953a1989ed665b497de24944431c35a590
Marcelo Zimbres <mzimbres@gmail.com>
2026-01-11 00:17:44 +0100

    Progresses with the benchamark script.

--- scripts/time.sh
+++ scripts/time.sh
@@ -2,6 +2,7 @@
 
 prefix=$(pwd)/tmp
 datadir="$prefix/data"
+rm -rf $datadir
 mkdir -p $datadir
 
 # See man 1 time
@@ -19,7 +20,11 @@ mkdir -p $datadir
 format="%e %S %U %P %M %R %c %w"
 
 #apps="boost_redis_co boost_redis_cb redis_rs go_redis rueidis"
-apps="boost_redis_co boost_redis_cb redis_rs go_redis"
+apps="boost_redis_co boost_redis_cb redis_rs"
+
+echo "========================================================================="
+echo "time"
+echo ""
 
 file=$datadir/time.txt
 echo "client $format"  > $file
@@ -29,32 +34,86 @@ for app in $apps; do
    sleep 1
 done
 
+column --table $file
+
+echo "========================================================================="
+echo "pidstat -u"
+echo ""
+
+file=$datadir/pidstat-u.txt
+echo "Type UID PID %usr %system %guest %wait %CPU CPU Command" > $file
 for app in $apps; do
-   file=$datadir/strace-$app.txt
-   rm -f $file
-   printf "%s " $app >> $file
-   strace --summary-only --output $file --summary-sort-by name $prefix/bin/app_$app
+   pidstat -u 1 -e $prefix/bin/app_$app | grep '^Average' >> $file
    sleep 1
 done
 
+column --table $file
+
+echo "========================================================================="
+echo "pidstat -v"
+echo ""
+
+file=$datadir/pidstat-v.txt
+echo "Type UID PID threads fd-nr Command" > $file
 for app in $apps; do
-   file=$datadir/pidstat-$app.txt
-   rm -f $file
-   pidstat -R 1 -e $prefix/bin/app_$app >> $file
-   sleep 1
-   pidstat -r 1 -e $prefix/bin/app_$app >> $file
-   sleep 1
-   pidstat -u 1 -e $prefix/bin/app_$app >> $file
-   sleep 1
-   pidstat -v 1 -e $prefix/bin/app_$app >> $file
-   sleep 1
-   pidstat -w 1 -e $prefix/bin/app_$app >> $file
+   pidstat -v 1 -e $prefix/bin/app_$app | grep '^Average' >> $file
    sleep 1
 done
 
+column --table $file
+
+echo "========================================================================="
+echo "pidstat -w"
+echo ""
+
+file=$datadir/pidstat-w.txt
+echo "Type UID PID cswch/s nvcswch/s Command" > $file
+for app in $apps; do
+   pidstat -w 1 -e $prefix/bin/app_$app | grep '^Average' >> $file
+   sleep 1
+done
+
+column --table $file
+
+echo "========================================================================="
+echo "pidstat -r"
+echo ""
+
+file=$datadir/pidstat-r.txt
+echo "Type UID PID minflt/s majflt/s VSZ RSS %MEM Command" > $file
+for app in $apps; do
+   pidstat -r 1 -e $prefix/bin/app_$app | grep '^Average' >> $file
+   sleep 1
+done
+
+column --table $file
+
+echo "========================================================================="
+echo "perf-stat"
+echo ""
+
+perf_res=$datadir/perf-stat.txt
+echo "client cache-misses branch-misses" > $perf_res
 for app in $apps; do
    file=$datadir/perf-stat-$app.txt
    rm -f $file
-   perf stat --output $file -e -- $prefix/bin/app_$app
+   perf stat -B -e branch-misses,cache-misses --output $file -- $prefix/bin/app_$app
+   cache_misses=$(grep cache-misses $file | awk '{print $1}')
+   branch_misses=$(grep branch-misses $file | awk '{print $1}')
+   echo "$app $cache_misses $branch_misses" >> $perf_res
    sleep 1
 done
+
+column --table $perf_res
+
+#echo "========================================================================="
+#echo "strace"
+#echo ""
+#
+#for app in $apps; do
+#   file=$datadir/strace-$app.txt
+#   rm -f $file
+#   printf "%s " $app >> $file
+#   strace --summary-only --output $file --summary-sort-by name $prefix/bin/app_$app
+#   sleep 1
+#done
